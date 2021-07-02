a3451068abaeb43cb1ee5d2c5c1a6ae187865fa5
Hema-Gantasala <80663245+Hema-Gantasala@users.noreply.github.com>
2021-07-02 23:04:00 +0530

    Create Programmer.java

---
+++ Programmer.java
@@ -0,0 +1,12 @@
+ final class Programmer extends Employee
+{   
+      Manager()
+       {
+       sal=40000;
+       Designation="Programmer";
+       }
+    public void raiseSal()
+     {
+        sal=sal+(sal*10)/100;
+     }
+} 
